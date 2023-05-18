fd325386d07739f37a0cb15f5f71e859dd155c7f
Adam Syamsuddin <133876172+adamsym@users.noreply.github.com>
2023-05-18 17:53:26 +0300

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,74 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim QueueArray(9) As String
+        Dim headpointer As Integer = 0
+        Dim tailpointer As Integer = 0
+        Dim nooofitems As Integer = 0
+        Dim userinput As String
+
+        For x = 0 To 10
+            Console.WriteLine("enter a string")
+            userinput = Console.ReadLine()
+            If enqueue(QueueArray, headpointer, tailpointer, nooofitems, userinput) = True Then
+                Console.WriteLine("successfull bruh")
+
+            Else
+                Console.WriteLine("unsuccessfullll bruh")
+
+            End If
+
+        Next
+
+        Console.WriteLine(dequeue(QueueArray, headpointer, tailpointer, nooofitems))
+        Console.WriteLine(dequeue(QueueArray, headpointer, tailpointer, nooofitems))
+
+
+        Console.ReadLine()
+    End Sub
+
+    Function enqueue(ByRef queuearray() As String, ByRef headpointer As Integer, ByRef TailPointer As Integer, ByRef NumberItems As Integer,
+            DataToAdd As String) As Boolean
+
+        If NumberItems = 10 Then
+            Return False
+        End If
+
+        queuearray(TailPointer) = DataToAdd
+        If TailPointer >= 9 Then
+            TailPointer = 0
+        Else
+            TailPointer = TailPointer + 1
+        End If
+
+        NumberItems = NumberItems + 1
+
+        Return True
+
+    End Function
+
+    Function dequeue(ByRef queuearray() As String, ByRef headpointer As Integer, ByRef TailPointer As Integer, ByRef NumberItems As Integer) As String
+
+
+        If NumberItems = 0 Then
+            Return "false"
+        Else
+            Dim returnvalue = queuearray(headpointer)
+            headpointer = headpointer + 1
+
+            If headpointer >= 9 Then
+                headpointer = 0
+
+            End If
+
+            NumberItems = NumberItems - 1
+
+            Return returnvalue
+
+        End If
+
+    End Function
+
+
+End Module
